e3fb8a22e0eed4ffaf6aa89741d6c2395e6899ac
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-08-05 19:51:28 +0200

    Episode #3 - Post Requests

--- src/index.js
+++ src/index.js
@@ -3,21 +3,32 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
-app.get("/groceries", (request, response) => {
-  response.send([
-    {
-      item: "milk",
-      quantity: 2,
-    },
-    {
-      item: "cereal",
-      quantity: 1,
-    },
-    {
-      item: "pop-tarts",
-      quantity: 1,
-    },
-  ]);
+const groceryList = [
+  {
+    item: 'milk',
+    quantity: 2,
+  },
+  {
+    item: 'cereal',
+    quantity: 1,
+  },
+  {
+    item: 'pop-tarts',
+    quantity: 1,
+  },
+];
+
+app.get('/groceries', (request, response) => {
+  response.send(groceryList);
+});
+
+app.post('/groceries', (request, response) => {
+  console.log(request.body);
+  groceryList.push(request.body);
+  response.send(201);
 });
