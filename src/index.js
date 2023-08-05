6fc1aaf5316c5fb7aa336d47aaa375d28dea4c30
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-08-05 15:53:48 +0200

    Episode #2 - GET Request + Deploy GCP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3001;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
+
+app.get("/groceries", (request, response) => {
+  response.send([
+    {
+      item: "milk",
+      quantity: 2,
+    },
+    {
+      item: "cereal",
+      quantity: 1,
+    },
+    {
+      item: "pop-tarts",
+      quantity: 1,
+    },
+  ]);
+});
